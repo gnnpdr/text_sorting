57cbec72ebc9d123a6062d93b9e65e8a38185318
markova-anna <markova.anna@phystech.edu>
2024-09-10 00:59:46 +0300

    file enter

---
+++ file.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+const int AMOUNT_OF_STR = 14;
+const int AMOUNT_OF_ELEMENTS_IN_STR = 200;
+
+void print(char text[][AMOUNT_OF_ELEMENTS_IN_STR]);
+
+
+
+int main()
+{
+   
+    char text[AMOUNT_OF_STR][AMOUNT_OF_ELEMENTS_IN_STR] = {};
+
+    char buffer[200];
+
+    FILE *fp = fopen("text.txt", "r");
+
+    if(fp)
+    {
+        char ch = 0;
+        //printf("all right\n");
+        while(ch != EOF)
+        {
+            for (int i = 0; i < AMOUNT_OF_STR; i++)
+            {
+                for (int j = 0; j < AMOUNT_OF_ELEMENTS_IN_STR; j++)
+                {
+                    ch = getc(fp);
+
+                    if (ch == '\0')
+                        break;
+
+                    text[i][j] = ch;
+
+                    printf("%c, row %d, str %d\n", text[i][j], i, j);
+                }
+            }
+        }
+    } 
+
+    print(text);
+    fclose(fp);
+    return 0;
+}
+
+void print(char text[][AMOUNT_OF_ELEMENTS_IN_STR])
+{
+    for (size_t str = 0; str < AMOUNT_OF_STR; str++)
+    {
+        int i = 0;
+        do
+        {
+            printf("%c", text[str][i]);
+            i++;
+        }
+        while(text[str][i] != '\0');
+        i = 0;
+    }
+
+}
