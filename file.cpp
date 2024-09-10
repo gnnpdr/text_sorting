265dcfb266d3f4a73810bcf673ca1c3221ec3c6e
markova-anna <markova.anna@phystech.edu>
2024-09-10 20:00:44 +0300

    Output with str problem (\0). Separate array

--- file.cpp
+++ file.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-//const int AMOUNT_OF_STR = 14;
-//const int AMOUNT_OF_ELEMENTS_IN_STR = 200;
+const int AMOUNT_OF_STR = 14;
 
-void print(char* text, long file_size);
+void print(char** addresses, int AMOUNT_OF_STR);
+void finding_str(char* text, long file_size, char** addresses);
 
 int main()
 {
@@ -14,21 +14,43 @@ int main()
     rewind (file_pointer);
 
     char* text = (char*)calloc(file_size, sizeof(char));
+    char** addresses = (char**)calloc(AMOUNT_OF_STR, sizeof(char*));
+    finding_str(text, file_size, addresses);
     
     fread (text, sizeof(char), file_size, file_pointer);
 
-    print(text, file_size);
+    print(addresses, file_size);
     free(text);
     return 0;
 }
 
-void print(char* text, long file_size)
+void print(char** addresses, int AMOUNT_OF_STR)
+{
+    size_t str = 0;
+    while (str < AMOUNT_OF_STR)
+    {
+        //printf("new str\n");
+        char* s = *(addresses + str);
+        printf("%s", s);
+        str++;
+    }
+}
+
+void finding_str(char* text, long file_size, char** addresses)
 {
     size_t element = 0;
+
+    int i = 0;
+    *(addresses + i) = text + element;
+
     while (element < file_size)
     {
         char* ch = text + element;
-        printf("%c", *ch);
+        if (*ch == '\n')
+        {
+            *(addresses + i) = text + element + 1;
+            i++;
+        }
         element++;
     }
 }
